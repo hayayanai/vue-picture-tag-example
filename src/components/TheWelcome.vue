7db74f0495bfceae4a419aae27cbcb0e25c7b8fa
hayayanai <92288373+hayayanai@users.noreply.github.com>
2024-09-16 14:46:35 +0900

    Add alt

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,8 +3,12 @@ import VPicture from './VPicture.vue'
 </script>
 
 <template>
-  <VPicture src="assets/images/l_01_square_white.png" class="pic" />
-  <VPicture src="assets/images/l_02_rectangle_white.png" class="pic" />
+  <VPicture src="assets/images/l_01_square_white.png" alt="square white sample image" class="pic" />
+  <VPicture
+    src="assets/images/l_02_rectangle_white.png"
+    alt="rectangle white sample image"
+    class="pic"
+  />
 </template>
 
 <style scoped>
